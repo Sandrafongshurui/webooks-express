bf86445e67b3a3e99f923bffa2ade79329cd835f
Sandrafongshurui <sandrafong90@hotmail.com>
2022-10-27 22:09:39 +0800

    fix:domain

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -73,6 +73,7 @@ const authController = {
     };
     const options = {
       httpOnly: true, //cookie can’t be read using JavaScript
+      domain: "w-ebooks.netlify.app",
       secure: true, //looking for https
       sameSite: "None",
     };
