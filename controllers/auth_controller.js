1679fc49342db175031a985331a78d7509050dfb
Sandrafongshurui <sandrafong90@hotmail.com>
2022-10-27 16:10:41 +0800

    fix: samesite:none

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -82,6 +82,7 @@ const authController = {
     return res
       .cookie("token", token, {
         httpOnly: true, //cookie can’t be read using JavaScript
+        sameSite: "none",
         secure: false //looking for https
       })
       .status(200)
